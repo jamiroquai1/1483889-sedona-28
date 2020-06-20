d5bf53ce54cd23a1c9976283f0b45f1fbb814a67
jamiroquai1 <mboy@narod.ru>
2020-06-20 22:54:23 +0300

    33

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-function myFunction() {
-    var element = document.querySelector(".modal");
-    element.classList.toggle("active");
-}
 
+(function () {
+    var form = document.querySelector('.form_order')
+    var openModalbtn = document.querySelector('.section_order_button');
+    form.classList.remove('form_order_active');
+    openModalbtn.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        form.classList.toggle('form_order_active');
+    })
+}());
